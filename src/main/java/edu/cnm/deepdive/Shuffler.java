5de6cd39ad4e5d32a0ff880d689115f37c760230
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-05 19:06:57 -0400

    Added helper method and overloads (in-progress).

--- src/main/java/edu/cnm/deepdive/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/Shuffler.java
@@ -12,7 +12,26 @@ public class Shuffler {
   }
 
   public void shuffle(int[] data) {
-    for (int sourceIndex = data.length-1; sourceIndex > 0; sourceIndex--) {
+    shufflePart(data, data.length-1);
+  }
+
+  public int[] shuffle(int[] data, int drawSize) {
+    shufflePart(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public double[] shuffle(double[] data, int drawSize) {
+    shufflePart(data, drawSize);
+    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
+  }
+
+  public void shuffle(double[] data) {
+    shufflePart(data, data.length-1);
+  }
+
+  // TODO Create overloads of both forms of shuffle (full and partial) for boolean[], byte[], char[], short[], float[], long[], and Object array
+  private void shufflePart(int[] data, int drawSize) {
+    for (int sourceIndex = data.length-1; sourceIndex >= data.length - drawSize; sourceIndex--) {
       int destIndex = rng.nextInt(sourceIndex + 1);
       if (sourceIndex != destIndex) {
         int temp = data[sourceIndex];
@@ -22,15 +41,17 @@ public class Shuffler {
     }
   }
 
-  public int[] shuffle(int[] data, int drawSize) {
+  private void shufflePart(double[] data, int drawSize) {
     for (int sourceIndex = data.length-1; sourceIndex >= data.length - drawSize; sourceIndex--) {
       int destIndex = rng.nextInt(sourceIndex + 1);
       if (sourceIndex != destIndex) {
-        int temp = data[sourceIndex];
+        double temp = data[sourceIndex];
         data[sourceIndex] = data[destIndex];
         data[destIndex] = temp;
       }
     }
-    return Arrays.copyOfRange(data, data.length - drawSize, data.length);
   }
+
+  // TODO Create overloads of shufflePart for boolean[], byte[], char[], short[], float[], long[], and Object array
 }
+
